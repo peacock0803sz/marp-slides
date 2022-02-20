82552267d680c8a7dcd46c5bccd3cc5ec59abbec
Yoichi TAKAI <takai@cmscom.jp>
2022-02-21 00:50:23 +0900

    :construction_worker: WIP: Create draft

---
+++ src/pycon-us-2022/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout h-full default">
+    <div class="pt-18">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ src/pycon-us-2022/layouts/subtitle.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout h-full grid subtitle">
+    <div class="pt-18">
+      <slot />
+    </div>
+  </div>
+</template>
